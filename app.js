4aec28b7b22d988a9456980c61a8b7f71dac852a
Alexander Kriz <82934078+AlexanderKriz@users.noreply.github.com>
2022-12-22 08:59:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Client = require('rippled-ws-client')
+const {XrplClient} = require('xrpl-client')
 const {parseBalanceChanges} = require('ripple-lib-transactionparser')
 
 const app = async (account, cb, returnTx) => {
@@ -7,9 +7,9 @@ const app = async (account, cb, returnTx) => {
       result?.transactions.forEach(r => {
         const {tx, meta} = r
         let direction = 'other'
-        if (tx?.Account === account) direction = 'sent'
-        if (tx?.Destination === account) direction = 'received'
-        const moment = (new Date((tx.date + 946684800) * 1000)).toISOString()
+        if (tx?.Account === account) direction = 's'
+        if (tx?.Destination === account) direction = 'r'
+        const moment = (new Date((tx.date + 946684800) * 1000)).toISOString().split('.')[0] + 'Z'
         const balanceChanges = parseBalanceChanges(meta)
         if (Object.keys(balanceChanges).indexOf(account) > -1) {
           const mutations = balanceChanges[account]
@@ -18,24 +18,21 @@ const app = async (account, cb, returnTx) => {
               ? 'XRP'
               : `${mutation.counterparty}.${mutation.currency}`
 
-            const isFee = direction === 'sent' && Number(mutation.value) * -1 * 1000000 === Number(tx?.Fee)
+            const isFee = direction === 's' && Number(mutation.value) * -1 * 1000000 === Number(tx?.Fee)
               ? 1
               : 0
 
-              const fee = direction === 'sent'
+              const fee = direction === 's'
               ? Number(tx?.Fee) / 1000000 * -1
               : 0
 
             cb({
-              ledger: tx.ledger_index,
+              date: moment,
               direction: direction,
               txtype: tx.TransactionType,
-              date: moment,
               currency: currency,
               amount: mutation.value,
               is_fee: isFee,
-              fee: fee,
-              hash: tx.hash,
               _tx: returnTx ? tx : undefined,
               _meta: returnTx ? meta : undefined
             })
@@ -45,15 +42,13 @@ const app = async (account, cb, returnTx) => {
     }
   }
 
-  const client = await new Client('wss://xrplcluster.com', {
-    NoUserAgent: true
-  })
+  const client = new XrplClient('wss://xrpl.ws/')
 
   const getMore = async marker => {
     const result = await client.send({
       command: 'account_tx',
       account,
-      limit: 10,
+      limit: 100,
       marker
     })
   
@@ -71,15 +66,12 @@ const app = async (account, cb, returnTx) => {
 }
 
 const fields = [
-  'ledger',
+  'date',
   'direction',
   'txtype',
-  'date',
   'currency',
   'amount',
   'is_fee',
-  'fee',
-  'hash'
 ]
 
 module.exports = {
